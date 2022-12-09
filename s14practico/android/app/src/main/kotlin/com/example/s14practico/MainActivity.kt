8d4d75f555a77e91edcd24f626a54e5cd51cb8ad
ddelarosae <ddelarosae@uninorte.edu.co>
2022-12-09 13:43:10 -0500

    proyecto notas sqflite

---
+++ s14practico/android/app/src/main/kotlin/com/example/s14practico/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.s14practico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ s14sqlite/android/app/src/main/kotlin/com/example/s14sqlite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.s14sqlite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
